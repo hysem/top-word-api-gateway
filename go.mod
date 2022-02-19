5fba7a91f013c3ddd18105ed98532655d4444a8b
Hysem Saleem <hysem101@hotmail.com>
2022-02-19 10:55:42 +0530

    fixed tests

--- go.mod
+++ go.mod
@@ -4,18 +4,12 @@ go 1.18
 
 require (
 	github.com/hysem/top-word-service v0.0.0-20220217123833-ec28b5089634
-	github.com/onsi/ginkgo/v2 v2.1.3
-	github.com/onsi/gomega v1.18.1
 	github.com/stretchr/testify v1.7.0
 )
 
 require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/davecgh/go-spew v1.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/objx v0.1.0 // indirect
-	golang.org/x/net v0.0.0-20210428140749-89ef3d95e781 // indirect
-	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
-	golang.org/x/text v0.3.6 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
 )
---
+++ topword/handler_test.go
@@ -0,0 +1,105 @@
+package topword_test
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+
+	"github.com/hysem/top-word-api-gateway/mocks"
+	"github.com/hysem/top-word-api-gateway/topword"
+	st "github.com/hysem/top-word-service/topword"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+)
+
+type handlerMocks struct {
+	topWordServiceClient mocks.TopWordServiceClient
+}
+
+func (m *handlerMocks) assertExpectations(t *testing.T) {
+	m.topWordServiceClient.AssertExpectations(t)
+}
+func newHandler(t *testing.T) (*topword.Handler, *handlerMocks) {
+	m := handlerMocks{}
+	h := topword.NewHandler(&m.topWordServiceClient)
+	return h, &m
+}
+
+func TestFindTopWords(t *testing.T) {
+	const testParagraph = "paragraph to test"
+	var testResponse = []*st.WordInfo{{
+		Word:  "paragraph",
+		Count: 1,
+	}, {
+		Word:  "to",
+		Count: 1,
+	}, {
+		Word:  "test",
+		Count: 1,
+	}}
+	testCases := map[string]struct {
+		requestMethod  string
+		requestText    string
+		expectedStatus int
+		expectedBody   interface{}
+		setMocks       func(m *handlerMocks)
+	}{
+		`error case: invalid request method`: {
+			requestMethod:  http.MethodGet,
+			expectedStatus: http.StatusMethodNotAllowed,
+		},
+		`error case: failed to find top words`: {
+			requestMethod: http.MethodPost,
+			requestText:   testParagraph,
+			setMocks: func(m *handlerMocks) {
+				m.topWordServiceClient.On("FindTopWords", mock.Anything, &st.FindTopWordsRequest{
+					Text: testParagraph,
+				}).Return(nil, assert.AnError)
+			},
+			expectedStatus: http.StatusInternalServerError,
+		},
+		`success case: found top words`: {
+			requestMethod: http.MethodPost,
+			requestText:   testParagraph,
+			setMocks: func(m *handlerMocks) {
+				m.topWordServiceClient.On("FindTopWords", mock.Anything, &st.FindTopWordsRequest{
+					Text: testParagraph,
+				}).Return(testResponse, nil)
+			},
+			expectedStatus: http.StatusOK,
+			expectedBody:   testResponse,
+		},
+	}
+	for name, tc := range testCases {
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			h, m := newHandler(t)
+			defer m.assertExpectations(t)
+			if tc.setMocks != nil {
+				tc.setMocks(m)
+			}
+
+			form := url.Values{}
+			form.Add("text", tc.requestText)
+
+			req, err := http.NewRequest(tc.requestMethod, "/endpoint", nil)
+			require.NoError(t, err)
+			req.PostForm = form
+			rec := httptest.NewRecorder()
+			h.FindTopWords(rec, req)
+
+			require.Equal(t, tc.expectedStatus, rec.Result().StatusCode)
+			if tc.expectedBody != nil {
+				var actualResponse []*st.WordInfo
+				err := json.NewDecoder(rec.Body).Decode(&actualResponse)
+				require.NoError(t, err)
+				require.Equal(t, actualResponse, testResponse)
+			} else {
+				require.Empty(t, rec.Body.String())
+			}
+		})
+	}
+}
