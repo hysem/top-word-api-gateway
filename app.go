f986b21285269d9d981f398744adab96a1f3163c
Hysem Saleem <hysem101@hotmail.com>
2022-02-19 07:22:25 +0530

    added comments

--- app.go
+++ app.go
@@ -13,20 +13,26 @@ import (
 const N = 10
 
 func main() {
+	// initialize the dependencies
 	topwordServiceClient := client.NewClient(getEnv("TOP_WORD_SERVICE", "http://localhost:8080"))
 	topwordHandler := topword.NewHandler(topwordServiceClient)
 
+	// sticking with the builtin http mux to have 0 dependencies
 	mux := http.NewServeMux()
 	mux.HandleFunc("/top-words", topwordHandler.FindTopWords)
 
+	// get the address from env or use the default one;
+	// can be replaced with viper or envconfig packages; but remember 0 dependencies
 	address := getEnv("ADDRESS", "0.0.0.0:8081")
 	log.Printf("listening@%s\n", address)
 
+	// Start the server and listen for requests
 	if err := http.ListenAndServe(address, mux); err != nil {
 		log.Fatalln("failed to start server")
 	}
 }
 
+// getEnv returns the value of a the given env. If it is empty or not defined then returns the default value specified
 func getEnv(key string, def string) string {
 	v := os.Getenv(strings.ToUpper(key))
 	if v == "" {
--- topword/handler.go
+++ topword/handler.go
@@ -2,43 +2,55 @@ package topword
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 
 	"github.com/hysem/top-word-service/client"
 	"github.com/hysem/top-word-service/topword"
 )
 
+// Handler implementation
 type Handler struct {
 	topWordServiceClient client.Client
 }
 
+// NewHandler returns an instance of Handler implementation
 func NewHandler(topWordServiceClient client.Client) *Handler {
 	return &Handler{
 		topWordServiceClient: topWordServiceClient,
 	}
 }
 
+// FindTopWords handles the request for finding the top words
 func (h *Handler) FindTopWords(rw http.ResponseWriter, r *http.Request) {
+	// Step 1: POST method allows sending far more data than the GET method so allowing only post method here
 	if r.Method != http.MethodPost {
 		rw.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
 
 	var request topword.FindTopWordsRequest
+	// Step 2: Get the text from form field data
 	request.Text = r.FormValue("text")
 
+	// Step 3: Find the top words using the client for top word service
 	topWords, err := h.topWordServiceClient.FindTopWords(r.Context(), &request)
 	if err != nil {
+		log.Println("failed to find top words", err)
 		rw.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
+	// Step 4: Marshal the results
 	b, err := json.Marshal(topWords)
+	// Step 5: Handle failure
 	if err != nil {
+		log.Println("failed to marshal top words", err)
 		rw.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
+	// Step 6: Write response
 	rw.Header().Add("Content-Type", "application/json")
 	rw.WriteHeader(http.StatusOK)
 	rw.Write(b)
